6ad5fbaea74af5952580fc98f120ca0a0099192f
mjayel <maksjl01@gmail.com>
2019-04-09 09:29:30 +0100

    Updated classifying video file (Main Script)

--- cut_video.py
+++ cut_video.py
@@ -1,5 +1,6 @@
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import subprocess
+import argparse
 import random
 import cv2
 import sys
@@ -7,23 +8,30 @@ import os
 
 VIDEO_FOLDER = "videos/"
 
+ap = argparse.ArgumentParser()
+ap.add_argument('-s', '--start_time', required=True)
+ap.add_argument('-d', '--duration', required=True)
+ap.add_argument('-f', '--filename', required=True)
+ap.add_argument('-g', '--get_images', type=bool)
+ap.add_argument('-c', '--count', type=float)
+ap.add_argument('-a', '--ad')
+args = vars(ap.parse_args())
+
 print('*' * 32)
 print('\n\tArguments: \n\t Start Time (Hours:Minutes:Seconds)\n\t End Time (Same) \n\t Cut Images (Bool) \n\t Image Cut Percentage (float) \n\t Ad Section (Bool)\n')
 print('*' * 32)
 
-filename, start_time, end_time = sys.argv[1], sys.argv[2], sys.argv[3]
+filename, start_time, end_time = args['filename'], args['start_time'], args['duration'] 
 #start/end time is in the format '14.30' in minutes and subclip takes it in seconds so multiply mins by 60
 
 #if you want to generate images is the section being cut an ad section or not ads
 image_extra_path = ''
 get_images = False
-random_percentage = ''
-if len(sys.argv) > 3:
-	get_images = True if sys.argv[4] == 'True' else False
-	random_percentage = sys.argv[5]
-	image_extra_path = 'ad' if sys.argv[6] == 'True' else 'notad'
-	#percentage is in 1-100 and random is between 0 and 1 so / 100
-	random_percentage = float(random_percentage) / 100
+every_second = 0
+if len(args) > 3:
+    get_images = args['get_images']
+    every_second = args['count']
+    image_extra_path = 'ad' if args['ad'].strip() == 'True' else 'notad'
 	
 
 video_folder = os.path.join(os.getcwd(), VIDEO_FOLDER)
@@ -41,17 +49,26 @@ subprocess.call(['ffmpeg', '-ss', f'{start_time}','-i',  f'{filename}','-to',f'{
     '-c','copy', f'videos/cuts/{video_name}.mp4'])
 
 if get_images:
-    print("[INFO] Cutting Images...")
-    vidcap = cv2.VideoCapture(new_video_path)
-    current_frame = 0
-    while(True):
-        ret,frame = vidcap.read()
-        if ret:
-            if random.random() < random_percentage:
-                cv2.imwrite('images/{}/{}'.format(image_extra_path, str(current_frame) + ".jpg"), frame)
-            current_frame+=1
-        else:
-            break
-	# release video and destroy process	
-    vidcap.release()
-    cv2.destroyAllWindows()
+    video_f = VideoFileClip(new_video_path) 
+    total_frame_count = video_f.duration
+    # Calculate how many images i need to cut (total seconds/images to cut per seconds)
+    image_cut_count = int(total_frame_count/every_second)
+    print(f'[STATUS] Cutting {image_cut_count} Images')
+    for image in range(image_cut_count):
+        print(f'\r{image_cut_count-image} Images Left', end='')
+        video_f.save_frame(f'images/{image_extra_path}/{os.path.basename(filename)[0:3]}{random.randrange(1,1000000)}.jpg', t=image*every_second)
+
+   # print("[INFO] Cutting Images...")
+   # vidcap = cv2.VideoCapture(new_video_path)
+   # current_frame = 0
+   # while(True):
+   #     ret,frame = vidcap.read()
+   #     if ret:
+   #         if random.random() < random_percentage:
+   #             cv2.imwrite('images/{}/{}'.format(image_extra_path, str(current_frame) + ".jpg"), frame)
+   #         current_frame+=1
+   #     else:
+   #         break
+   #     # release video and destroy process	
+   # vidcap.release()
+   # cv2.destroyAllWindows()
--- video_classify.py
+++ video_classify.py
@@ -2,11 +2,17 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 from multiprocessing.dummy import Pool as ThreadPool
 from keras.models import load_model
 import matplotlib.pyplot as plt
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import numpy as np
+import subprocess
 import argparse
 import cv2
 import os
+import re
+
+"""
+    Main File which takes a video as input and cuts the AD sections out
+"""
 
 ASPECT_RATIO = 1080/1920
 IMAGE_DIMS = (128,int(128*ASPECT_RATIO))
@@ -15,39 +21,30 @@ ap=argparse.ArgumentParser()
 ap.add_argument('-v', '--video', required=True)
 ap.add_argument('-c', '--count', type=int, required=True)
 ap.add_argument('-m', '--model', required=True)
+ap.add_argument('-o', '--offset')
 args = vars(ap.parse_args())
 
-print(f"Cutting images every {args['count']} seconds...")
+print(f'[STATUS] Cutting images every {args["count"]} seconds')
 
 # Load video
-video_f = VideoFileClip(args["video"])
-
-vidcap = cv2.VideoCapture(args['video'])
-current_frame = 0
+video_f = VideoFileClip(args['video'])
 image_data = []
+multiplier = args['count']
 
-multiplier = args['count'] * round(vidcap.get(cv2.CAP_PROP_FPS))
-total_frame_count = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
-print(f"[INFO] Multiplier: {multiplier}")
-print(f"[INFO] FPS: {round(vidcap.get(cv2.CAP_PROP_FPS))}")
-print(f"[INFO] Total Frames: {total_frame_count}")
+total_frame_count = video_f.duration
+image_cut_count = int(total_frame_count/multiplier)
+for x in range(image_cut_count):
+    video_f.save_frame(f'tmp/{str(x)}.jpg', t=x*multiplier)
 
-if os.path.isfile(f"tmp/{str(current_frame)}.jpg") != True:
-    print('[INFO] Cutting...')
-    while True:
-        ret,frame = vidcap.read()
-        if ret:
-            # If current frame is correct offset e.g. 10s then save data
-            if current_frame % multiplier == 0:
-                cv2.imwrite('tmp/{}'.format(str(current_frame)+'.jpg'), frame)
-            current_frame += 1
-    vidcap.release()
-    cv2.destroyAllWindows()
-
-print(f"[INFO] Pre-processing Image Data...")
+print(f'[STATUS] Pre-processing Image Data')
+pattern = re.compile('([0-9]*?).jpg')
 tmp_dir = os.path.join(os.getcwd(), 'tmp')
-images = [os.path.join(tmp_dir,x) for x in os.listdir('tmp/') if 'DS_Store' not in x]
-print(f"[INFO] {len(images)} Images")
+# Sort the images in order of their number
+images = [x for x in os.listdir('tmp/') if 'DS_Store' not in x]
+images = sorted(images, key=lambda x: int(re.search(pattern, x).group(1)))
+images = [os.path.join(tmp_dir, x) for x in images]
+
+print(f'[INFO] {len(images)} Images')
 # Preprocess image data
 for image in images:
     img = cv2.imread(image, 0)
@@ -61,21 +58,87 @@ image_data = np.array(image_data) / 255.0
 # Reshape from (65, image_dims) to (65, image_dims, 1) -> add color channel
 image_data = image_data.reshape(image_data.shape[0], IMAGE_DIMS[0], IMAGE_DIMS[1], -1)
 
-cut_times = []
+# First section will always be an AD
+cut_times = [(0, False)]
 
 # Load keras model
 model = load_model(args['model'])
-
 predictions = model.predict(image_data)
 
+# If its not an AD and it was previously append Current Time, True to 
+# cut_times array, if it was not previously and AD and now is append
+# Current Time, False to the array
+prev_ad = False
+count = 0
+ad_count = 0
+
 for x in range(len(images)):
-    #current_img = image_data[x].reshape(IMAGE_DIMS[0], IMAGE_DIMS[1])
-    label = str(np.argmax(predictions[x]))
-    img = np.array(Image.open(images[x]))
-    cv2.putText(img, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,
-	0.7, (255,255,255))
-    cv2.imshow('Output', img, cmap='gray')
-    cv2.waitKey(0)
+    label = int(np.argmax(predictions[x]))
+
+    if label == 0:
+        ad_count = 0
+        if prev_ad or (count > 0 and count < 2):
+            count += 1
+            if count == 2:
+                cut_times.append(((x-3)*multiplier, True))
+    if label == 1:
+        count = 0
+        if not prev_ad or ad_count > 0:
+            if ad_count == 1:
+                cut_times.append(((x-1)*multiplier, False))
+            ad_count += 1
+    prev_ad = label
+
+print(cut_times)
+
+def cut_video_part(start_time, end_time, filename, output_filename):
+    subprocess.call(['ffmpeg', '-ss', f'{start_time}', '-i', f'{filename}', '-to', f'{end_time}', 
+        '-c', 'copy', f'videos/final_videos/{output_filename}.mp4'])
+
+
+for x in cut_times:
+    label = x[1]
+    img = Image.open(images[int(x[0]/multiplier)])
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype('/Libary/Fonts/Courier New.ttf', 100)
+    draw.text((0,0), str(label), (255,255,255), font=font)
+    plt.imshow(img)
+    plt.show()
+
+# Cut the video
+start_time = 0
+end_time = 0
+for x in range(1, len(cut_times)):
+    end_time = cut_times[x][0]
+    cut_video_part(start_time, int(end_time-start_time), args['video'], str(x)) 
+    start_time = cut_times[x][0]
+
+final_videos = []
+cut_videos = [x for x in os.listdir(os.path.join(os.getcwd(), 'videos', 'final_videos')) if '.DS_Store' not in x]
+
+for x in range(len(cut_videos)):
+    if not cut_times[x][1]:
+        final_videos.append(cut_videos[x])
+
+print(final_videos)
+
+removable = set(cut_videos) - set(final_videos)
+for remove in removable:
+    os.remove(os.path.join(os.getcwd(), 'videos' ,'final_videos', remove))
+
+#print('Removed ad videos')
+
+
+# Look at images and their label
+#for x in range(len(images)):
+#    #current_img = image_data[x].reshape(IMAGE_DIMS[0], IMAGE_DIMS[1])
+#    label = str(np.argmax(predictions[x]))
+#    img = Image.open(images[x])
+#    draw = ImageDraw.Draw(img)
+#    font =  ImageFont.truetype('/Libary/Fonts/Courier New.ttf', 100)
+#    draw.text((0,0), label, (255,255,255), font=font)
+#    plt.imshow(img)
+#    plt.show()
 
 for x in images:
     os.remove(x)
